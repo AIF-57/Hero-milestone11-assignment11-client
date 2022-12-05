306066a0569528e204ae97261c46843309473ea2
ariyan-islam <afislam57@gmail.com>
2022-12-06 03:47:29 +0600

    blogs route added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navbar from './Pages/Shared/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
+import Blogs from './Pages/Blogs/Blogs';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Routes>
+        <Route path='blogs' element={<Blogs></Blogs>}/>
         <Route path='/login' element={<Login></Login>}/>
         <Route path='/register' element={<Register></Register>}/>
       </Routes>
---
+++ src/Pages/Blogs/Blogs.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div className='my-10'>
+            <div className="blogContainer m-5 flex flex-col items-center">
+                <div className="blog text-left w-[80%] my-5">
+                    <p className='text-2xl font-semibold my-2'>Difference between Node.JS and Javascript</p>
+                    <div className='content flex '>
+                        <span className='mx-2'>1.</span>
+                        <div className="line">
+                            <p>Javascript is a programming language</p>
+                            <p>NodeJS is a Javascript runtime</p>
+                        </div>
+                    </div>
+                    <div className='content flex'>
+                        <span className='mx-2'>2.</span>
+                        <div className="line">
+                            <p>Javascript can only run on the browser</p>
+                            <p>Javascript can run outside from the browser by NodeJS</p>
+                        </div>
+                    </div>
+                    <div className='content flex'>
+                        <span className='mx-2'>3.</span>
+                        <div className="line">
+                            <p>Javascript used to client-side in general</p>
+                            <p>Usually, NodeJS used for server-side</p>
+                        </div>
+                    </div>
+                    <div className='content flex'>
+                        <span className='mx-2'>4.</span>
+                        <div className="line">
+                            <p>Javascript is the updated version of the ECMA script which uses chrome's V8 engine</p>
+                            <p>C, C++, and Javascript are the languages used for NodeJS</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="blog text-left w-[80%] my-5">
+                    <p className='text-2xl font-semibold my-2'>Difference between SQL and NoSQL</p>
+                    <div className='content flex '>
+                        <span className='mx-2'>1.</span>
+                        <div className="line">
+                            <p>SQL databases are primarily called as Relational Databases RDBMS</p>
+                            <p>NoSQL database are primarily called as non-relational or distributed database</p>
+                        </div>
+                    </div>
+                    <div className='content flex'>
+                        <span className='mx-2'>2.</span>
+                        <div className="line">
+                            <p>SQL databases are table-based</p>
+                            <p>NoSQL databases are either key-value pairs, document-based, graph databases or wide-column stores</p>
+                        </div>
+                    </div>
+                    <div className='content flex'>
+                        <span className='mx-2'>3.</span>
+                        <div className="line">
+                            <p>SQL databases have fixed or static or predefined schema</p>
+                            <p>NoSQL have dynamic schema</p>
+                        </div>
+                    </div>
+                    <div className='content flex'>
+                        <span className='mx-2'>4.</span>
+                        <div className="line">
+                            <p>SQL vertically Scalable</p>
+                            <p>NoSQL horizontally scalable</p>
+                        </div>
+                    </div>
+                </div>
+            
+                <div className="blog text-left w-[80%] my-5">
+                    <p className='text-2xl font-semibold my-2'>What is the purpose of jwt and how does it work</p>
+                    <div className='content'>
+                        <div className="line">
+                        <p>JWTs or JSON Web Tokens are most commonly used to identify an authenticated user. They are issued by an authentication server and are consumed by the client-server (to secure its APIs).</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Blogs;
